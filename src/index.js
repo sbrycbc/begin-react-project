133501822ddfe30257772488cea213f497ca03c2
sbrycbc <sabriyecbc@gmail.com>
2023-05-25 16:26:24 +0200

    first React App and Component...

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(){
+    return <h1>Hallo!! My name is SbryCbc</h1>;
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")   //document.getElementById('root')
+);
