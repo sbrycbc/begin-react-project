9ead7859d68ab34f0ff407fe3341515343aa4ac3
sbrycbc <sabriyecbc@gmail.com>
2023-05-27 17:38:56 +0200

    dritte  Unterricht Props mit First Class...

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Card from './components/card';
+
 //-------------- Erste Unterricht------------//
 
 /* function App(){
     return <h1>Hallo!! My name is SbryCbc</h1>;
 }
   */
-/* class App2 extends React.Component {
+/* className App2 extends React.Component {
     render () {
         return <h1>Hallo!! My name is SbryCbc</h1>;
     }
@@ -28,15 +30,37 @@ import ReactDOM from 'react-dom';
     );      
 } */
 
-function App (){
+/* function App (){
     //const buttonValue = "My Button"
     const str1 = "Click";
     const str2 = "-Me!!";
     return(
         <div>
-            <button  tabIndex="3" className='jskCLASS' type='button' style={{padding:'10px', color: 'white', backgroundColor: 'blue', border:'2px solid yellow'}}>{str1.concat(str2)}</button>
+            <button  tabIndex="3" className='jskclassName' type='button' style={{padding:'10px', color: 'white', backgroundColor: 'blue', border:'2px solid yellow'}}>{str1.concat(str2)}</button>
         </div>
     )
+} */
+
+//-------------- Dritte Unterricht------------//
+
+// Props ---> Componentlar arasi veri iletisimini saglar. 
+
+
+const App = () => {
+
+    return (
+             <div>
+                <div className="card-group">
+
+                    <Card cardTitle="Lebenstmittel"/>
+
+                    <Card cardTitle="Sport-Lauf"/>
+
+                    <Card cardTitle="Laufen-Verrückt"/>
+
+                </div>
+            </div>
+    );
 }
 
 
